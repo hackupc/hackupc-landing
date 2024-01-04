030749cde380a82de5f666452128854c0587adb6
aacraf <aacraaf@gmail.com>
2024-01-04 13:59:25 -0500

    styled component in About

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,3 +1,29 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  border: 1px solid black;
+`;
+
 export default function About() {
-  return <div>This is the About :)</div>;
+  return (
+    <div>
+      <Card>
+        The word hackathon is a combination of the words "HACK", standing for
+        creative problem-solving, and "MARATHON". These are awesome events full
+        of creativity, technology, and passionate tech-related students of
+        different skill levels collaborating and creating. During a certain
+        period of time, attendees work in TEAMS to develop apps, games, robots…
+        literally anything you want to BUILD & LEARN about!
+      </Card>
+      <Card>
+        {" "}
+        HackUPC is the first student hackathon in Spain. Once a year, we aim to
+        provide a space for students of all experience levels from all around
+        the world where they can improve and learn new skills, meet awesome
+        people, and overall have a great experience! This year's edition will be
+        the first since the pandemic to have a FULLY IN-PERSON format, meaning
+        that you can only participate by physically attending the event.
+      </Card>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "./components/Hero";
