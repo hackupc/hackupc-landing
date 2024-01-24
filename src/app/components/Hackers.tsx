cfa9b4a942d6978cfbdbd46e508f4ff53324703f
EncryptEx <limusina10@gmail.com>
2024-01-24 22:30:03 +0100

    Struct done

--- src/app/components/Hackers.tsx
+++ src/app/components/Hackers.tsx
@@ -1,3 +1,48 @@
+import styled from "styled-components";
+import React from "react";
+const Title = styled.h1``;
+const Section = styled.div`
+  align-content: center;
+  text-align: center;
+`;
+const Description = styled.p`
+  padding: 10px;
+`;
+const Button = styled.a`
+  padding: 10px;
+  border-radius: 10px;
+`;
+
+const Image = styled.img`
+  width: ${(props) => props.width || "15%"};
+`;
+
 export default function Hackers() {
-  return <div>This is the Hackers :)</div>;
+  return (
+    <Section>
+      <Title>The organization behind HackUPC</Title>
+
+      <Description>
+        Hackers@UPC is a non-profit student organization at Universitat
+        Politècnica de Catalunya.
+      </Description>
+
+      <Image
+        src="/hackersatupc.png"
+        alt="Logotype of HackersAtUPC"
+        width="35%"
+      ></Image>
+
+      <Description>
+        Throughout the year, we organize national and international events in
+        order to promote science and technology among students and create a
+        great community in the technological world.
+      </Description>
+      <Description>
+        Our mission is to foster learning, designing, and building to turn
+        students' ideas into a reality!
+      </Description>
+      <Button href={"https://hackersatupc.org"}>Go to website</Button>
+    </Section>
+  );
 }
