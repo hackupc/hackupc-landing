947441946582373e5c524fc182ec8b1eb24a1342
Isaac <isaac.bassas@gmail.com>
2024-01-29 18:22:27 +0100

    Esquelet basic del live & Sponsors&Partners

--- src/app/components/Live.tsx
+++ src/app/components/Live.tsx
@@ -1,8 +1,61 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 20px;
+`;
+
+const TimeColumn = styled.div`
+  flex-direction: column;
+  padding-right: 20px;
+  padding-top: 75px;
+`;
+
+const ActivityColumn = styled.div`
+  width: 70%;
+  flex-direction: column;
+  padding-right: 20px;
+  padding-top: 75px;
+`;
+
+const TimeSlot = styled.div`
+  margin-bottom: 30px;
+`;
+
+const ActivitySlot = styled.div`
+  margin-bottom: 48px;
+`;
+
 export default function Live() {
+  const liveSchedule = [
+    { time: "15:00 17:45", activity: "Hacker Registration" },
+    { time: "15:00 15:00", activity: "Baggage Registration" },
+    { time: "18:00 19:30", activity: "Openning Ceremony" },
+    { time: "19:30 22:00", activity: "Dinner: Poke" },
+    { time: "20:00 20:20", activity: "Hacking Starts!!!" },
+    { time: "20:00 20:00", activity: "BieneCatcher" },
+  ];
+
   return (
-    <div>
-      This is the Live :)
-      <div></div>
-    </div>
+    <Container>
+      <h2>Friday</h2>
+      <TimeColumn>
+        {liveSchedule.map((item, index) => (
+          <TimeSlot key={index}>
+            <strong>{item.time}</strong>
+          </TimeSlot>
+        ))}
+      </TimeColumn>
+
+      <ActivityColumn>
+        {liveSchedule.map((item, index) => (
+          <ActivitySlot key={index}>
+            <h3>{item.activity}</h3>
+          </ActivitySlot>
+        ))}
+      </ActivityColumn>
+    </Container>
   );
 }
--- src/app/components/SponsorsAndPartners.tsx
+++ src/app/components/SponsorsAndPartners.tsx
@@ -1,3 +1,52 @@
-export default function SponsorsAndPartners() {
-  return <div>This is the SponsorsAndPartners :)</div>;
+import styled from "styled-components";
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  margin-top: 20px;
+`;
+
+const ImageGrid = styled.div`
+  display: flex;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  margin-top: 20px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+export default function MyWebpage() {
+  return (
+    <Container>
+      <Title>Sponsors</Title>
+      <ImageGrid>
+        <Image src="" width={50} height={50} alt="" />
+      </ImageGrid>
+
+      <ImageGrid>
+        <Image src="" width={50} height={50} alt="" />
+        <Image src="" width={50} height={50} alt="" />
+      </ImageGrid>
+
+      <ImageGrid>
+        <Image src="" width={50} height={50} alt="" />
+        <Image src="" width={50} height={50} alt="" />
+        <Image src="" width={50} height={50} alt="" />
+        <Image src="" width={50} height={50} alt="" />
+      </ImageGrid>
+
+      <Title>Partners</Title>
+      <ImageGrid>
+        <Image src="" width={50} height={50} alt="" />
+        <Image src="" width={50} height={50} alt="" />
+        <Image src="" width={50} height={50} alt="" />
+        <Image src="" width={50} height={50} alt="" />
+      </ImageGrid>
+    </Container>
+  );
 }
