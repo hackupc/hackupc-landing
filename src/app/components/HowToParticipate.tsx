055724f780e9dc93c432f52cb5662e71d78610a0
maui <hello@mauriciabad.com>
2024-02-04 17:06:38 +0100

    Fix format

--- src/app/components/HowToParticipate.tsx
+++ src/app/components/HowToParticipate.tsx
@@ -72,8 +72,7 @@ const Button = styled.button`
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
 
   &:hover {
-    background-color: ${(props) =>
-      props.color === "#D9AFFF" ? "#ac4dff" : "#9c1916"};
+    background-color: ${(props) => (props.color === "#D9AFFF" ? "#ac4dff" : "#9c1916")};
     color: #fff;
   }
 `;
