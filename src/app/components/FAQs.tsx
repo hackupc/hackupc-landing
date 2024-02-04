70761ed5589d7f8edade807aa0830dda269e6762
carlotacb <carkbra@gmail.com>
2024-02-04 16:55:11 +0100

    added all the faqs of about hackupc section

--- src/app/components/FAQs.tsx
+++ src/app/components/FAQs.tsx
@@ -86,6 +86,75 @@ export default function FAQs() {
                 A4, A5 and A6) in sunny Barcelona.
               </QuestionAnswer>
             </Question>
+            <Question>
+              <QuestionTitle>What does HackUPC provide?</QuestionTitle>
+              <QuestionAnswer>
+                HackUPC is free for all accepted hackers. We will provide you
+                with Wi-Fi, swag, workspaces, and meals (including breakfast,
+                lunch, dinner, and snacks during the night). There will be
+                access to the showers in a specific schedule and mattresses to
+                relax in sleep-enabled classrooms (there will be not one
+                mattress per person).
+              </QuestionAnswer>
+            </Question>
+            <Question>
+              <QuestionTitle>What can I build?</QuestionTitle>
+              <QuestionAnswer>
+                You can build any tech-related project. We do not have
+                restrictions over the topic. Some examples are web pages, mobile
+                apps, hardware projects, games, an API... Remember that you can
+                also use a variety of hardware components, from Arduino kits and
+                Raspberry Pi's, to Amazon Echo dots and Myo Arm.
+              </QuestionAnswer>
+            </Question>
+            <Question>
+              <QuestionTitle>What should I bring?</QuestionTitle>
+              <QuestionAnswer>
+                All attendees must bring the following:
+                <ul>
+                  <li>Valid government ID for admission. </li>
+                  <li>
+                    Laptop & charger (adapter if you don’t have the same plug as
+                    in Spain).
+                  </li>
+                </ul>
+                Other common checklist items include:
+                <ul>
+                  <li>Sleeping bag</li>
+                  <li>
+                    Towel, flip-flops, and soap, we encourage all the hackers to
+                    take a shower!
+                  </li>
+                </ul>
+                We will send you a more detailed checklist once you’re accepted.
+              </QuestionAnswer>
+            </Question>
+            <Question>
+              <QuestionTitle>What are the judging criteria?</QuestionTitle>
+              <QuestionAnswer>
+                Projects will mainly be judged based on technical difficulty,
+                innovation, overall learning experience, and also, on their
+                usability and design by a panel of selected judges from the
+                industry.
+              </QuestionAnswer>
+            </Question>
+            <Question>
+              <QuestionTitle>
+                What if I have any dietary restrictions?
+              </QuestionTitle>
+              <QuestionAnswer>
+                Since beta 1.0, we support vegan, vegetarian, gluten-free and
+                any type of allergies. Make sure you are specifying your dietary
+                in the application form.
+              </QuestionAnswer>
+            </Question>
+            <Question>
+              <QuestionTitle>Do you have a code of conduct?</QuestionTitle>
+              <QuestionAnswer>
+                Yes, here it is: Code of conduct. TL;DR: Be respectful,
+                harassment and abuse are never tolerated.
+              </QuestionAnswer>
+            </Question>
           </QuestionsBlock>
           <QuestionsBlock>
             <BlockTitle>Travel Reimbursement</BlockTitle>
