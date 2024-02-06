93a8a98025f6de6bfb942bd0c65f17778f7793d5
carlotacb <carkbra@gmail.com>
2024-02-06 19:46:49 +0100

    added interfaces in faqs and added icon

--- src/app/components/FAQs.tsx
+++ src/app/components/FAQs.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { hackupc_faqs } from "@/app/data/faqs_data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Section = styled.div`
   width: 100%;
@@ -55,6 +57,12 @@ const QuestionAnswer = styled.div`
   line-height: 24px;
 `;
 
+const QuestionTitleBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 export default function FAQs() {
   return (
     <Section>
@@ -63,98 +71,15 @@ export default function FAQs() {
         <ColumnsQuestions>
           <QuestionsBlock>
             <BlockTitle>About HackUPC</BlockTitle>
-            <Question>
-              <QuestionTitle>What is HackUPC?</QuestionTitle>
-              <QuestionAnswer>
-                HackUPC is the first student hackathon in Spain. This will be
-                our tenth edition with lots of side activities to learn new
-                technologies, meet interesting people and have fun. Come and
-                make history with us!
-              </QuestionAnswer>
-            </Question>
-            <Question>
-              <QuestionTitle>When is HackUPC?</QuestionTitle>
-              <QuestionAnswer>
-                The opening ceremony starts at 7:00 p.m. on Friday, May, 3rd. We
-                expect to finish by 5:00 p.m. on Sunday, May 5th.
-              </QuestionAnswer>
-            </Question>
-            <Question>
-              <QuestionTitle>Where is HackUPC?</QuestionTitle>
-              <QuestionAnswer>
-                HackUPC will take place in UPC’s North Campus FIB buildings (A3,
-                A4, A5 and A6) in sunny Barcelona.
-              </QuestionAnswer>
-            </Question>
-            <Question>
-              <QuestionTitle>What does HackUPC provide?</QuestionTitle>
-              <QuestionAnswer>
-                HackUPC is free for all accepted hackers. We will provide you
-                with Wi-Fi, swag, workspaces, and meals (including breakfast,
-                lunch, dinner, and snacks during the night). There will be
-                access to the showers in a specific schedule and mattresses to
-                relax in sleep-enabled classrooms (there will be not one
-                mattress per person).
-              </QuestionAnswer>
-            </Question>
-            <Question>
-              <QuestionTitle>What can I build?</QuestionTitle>
-              <QuestionAnswer>
-                You can build any tech-related project. We do not have
-                restrictions over the topic. Some examples are web pages, mobile
-                apps, hardware projects, games, an API... Remember that you can
-                also use a variety of hardware components, from Arduino kits and
-                Raspberry Pi's, to Amazon Echo dots and Myo Arm.
-              </QuestionAnswer>
-            </Question>
-            <Question>
-              <QuestionTitle>What should I bring?</QuestionTitle>
-              <QuestionAnswer>
-                All attendees must bring the following:
-                <ul>
-                  <li>Valid government ID for admission. </li>
-                  <li>
-                    Laptop & charger (adapter if you don’t have the same plug as
-                    in Spain).
-                  </li>
-                </ul>
-                Other common checklist items include:
-                <ul>
-                  <li>Sleeping bag</li>
-                  <li>
-                    Towel, flip-flops, and soap, we encourage all the hackers to
-                    take a shower!
-                  </li>
-                </ul>
-                We will send you a more detailed checklist once you’re accepted.
-              </QuestionAnswer>
-            </Question>
-            <Question>
-              <QuestionTitle>What are the judging criteria?</QuestionTitle>
-              <QuestionAnswer>
-                Projects will mainly be judged based on technical difficulty,
-                innovation, overall learning experience, and also, on their
-                usability and design by a panel of selected judges from the
-                industry.
-              </QuestionAnswer>
-            </Question>
-            <Question>
-              <QuestionTitle>
-                What if I have any dietary restrictions?
-              </QuestionTitle>
-              <QuestionAnswer>
-                Since beta 1.0, we support vegan, vegetarian, gluten-free and
-                any type of allergies. Make sure you are specifying your dietary
-                in the application form.
-              </QuestionAnswer>
-            </Question>
-            <Question>
-              <QuestionTitle>Do you have a code of conduct?</QuestionTitle>
-              <QuestionAnswer>
-                Yes, here it is: Code of conduct. TL;DR: Be respectful,
-                harassment and abuse are never tolerated.
-              </QuestionAnswer>
-            </Question>
+            {hackupc_faqs.map((faq) => (
+              <Question key={faq.question}>
+                <QuestionTitleBlock>
+                  <QuestionTitle>{faq.question}</QuestionTitle>
+                  <FontAwesomeIcon icon={["fas", "angle-down"]} />
+                </QuestionTitleBlock>
+                <QuestionAnswer>{faq.answer}</QuestionAnswer>
+              </Question>
+            ))}
           </QuestionsBlock>
           <QuestionsBlock>
             <BlockTitle>Travel Reimbursement</BlockTitle>
