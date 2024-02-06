89d57d1d546dca663965536f0706a2d20c528070
carlotacb <carkbra@gmail.com>
2024-02-06 19:46:37 +0100

    created interfaces

---
+++ src/app/data/faqs_data.ts
@@ -0,0 +1,73 @@
+import { FAQs } from "@/app/data/interfaces";
+
+export const hackupc_faqs: FAQs[] = [
+  {
+    id: 1,
+    question: "What is HackUPC?",
+    answer:
+      "HackUPC is the first student hackathon in Spain. This will be our tenth edition with lots of side activities to learn new technologies, meet interesting people and have fun. Come and make history with us!",
+  },
+  {
+    id: 2,
+    question: "When is HackUPC?",
+    answer:
+      "The opening ceremony starts at 7:00 p.m. on Friday, May, 3rd. We expect to close this year’s edition by 5:00 p.m. on Sunday, May 5th.",
+  },
+  {
+    id: 3,
+    question: "Where is HackUPC?",
+    answer:
+      "HackUPC will take place in UPC’s North Campus FIB buildings (A3, A4, A5 and A6) in sunny Barcelona. Expect around 25ºC!",
+  },
+  {
+    id: 4,
+    question: "What does HackUPC provide?",
+    answer:
+      "HackUPC is free for all accepted hackers. We will provide you with Wi-Fi, swag, workspaces with power outlets (Type F plug), and meals (including breakfast, lunch, dinner, and snacks during the night). There will be access to the showers in a specific schedule and mattresses to relax in the sleep-enabled classrooms (there will not be one mattress per person).",
+  },
+  {
+    id: 5,
+    question: "What can I build?",
+    answer:
+      "You can build any tech-related project. We do not have restrictions over the topic. Some examples are web pages, mobile apps, hardware projects, games, an API... Remember that you can also use a variety of hardware components, from Arduino kits and Raspberry Pi's, to Amazon Echo dots and Myo Arm.",
+  },
+  {
+    id: 6,
+    question: "What should I bring?",
+    answer:
+      "All attendees must bring the following: Valid government ID for admission and Laptop & charger (Be aware that Spain has Type F plugs, bring an adapter!). Other common checklist items include: Sleeping bag, Towel, flip-flops, and soap, we encourage all the hackers to take a shower! We will send you a more detailed checklist once you’re accepted.",
+  },
+  {
+    id: 7,
+    question: "What are the judging criteria?",
+    answer:
+      "Projects will mainly be judged based on technical difficulty, innovation, overall learning experience, and also, on their usability and design by a panel of selected judges from the industry.",
+  },
+  {
+    id: 8,
+    question: "What if I have any dietary restrictions?",
+    answer:
+      "Since beta 1.0, we support vegan, vegetarian, gluten-free and any type of allergies. As we always say, we’ve got you covered!",
+  },
+  {
+    id: 9,
+    question: "Do you have a code of conduct?",
+    answer:
+      "Yes, here it is: Code of conduct. TL;DR: Be respectful, harassment and abuse are never tolerated.",
+  },
+];
+
+export const travel_faqs: FAQs[] = [
+  {
+    id: 1,
+    question: "Is travel reimbursement available?",
+    answer:
+      "Of course! The quantity changes depending on where you come from. We are giving up to €50 for Spaniards outside of Catalonia, €120 if you are coming from Europe and up to €200 if you come from outside Europe. We want to give all of you the opportunity to join us! However, you must remember that not all participants will be eligible to receive travel reimbursement. In the end, we have a limited budget, and we want to help our participants, but we are a student organization and we have limited resources.",
+  },
+  {
+    id: 2,
+    question: "Is travel reimbursement available?",
+    answer:
+      "Of course! The quantity changes depending on where you come from. We are giving up to €50 for Spaniards outside of Catalonia, €120 if you are coming from Europe and up to €200 if you come from outside Europe. We want to give all of you the opportunity to join us! However, you must remember that not all participants will be eligible to receive travel reimbursement. In the end, we have a limited budget, and we want to help our participants, but we are a student organization and we have limited resources.",
+  },
+];
---
+++ src/app/data/interfaces.ts
@@ -0,0 +1,5 @@
+export interface FAQs {
+  id: number;
+  question: string;
+  answer: string;
+}
