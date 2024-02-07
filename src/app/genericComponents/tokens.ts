dea030636ab9804912bd46c9ef2eed027795d284
carlotacb <carkbra@gmail.com>
2024-02-07 01:31:59 +0100

    added more tokens

--- src/app/genericComponents/tokens.ts
+++ src/app/genericComponents/tokens.ts
@@ -1,12 +1,37 @@
 // Font sizes
+export const TitleL: string = "32px";
+export const TitleM: string = "26px";
+export const TitleS: string = "20px";
 export const BodyText: string = "18px";
-export const H1: string = "28px";
-export const H2: string = "25px";
-export const H3: string = "22px";
+export const BodyTextMedium: string = "16px";
+export const BodyTextSmall: string = "14px";
 
 // Colors
-export const PrimaryColor: string = "#E23532";
-export const SecondaryColor: string = "#D9AFFF";
+export const Primary500: string = "#240059";
+export const Primary400: string = "#410658";
+export const Primary300: string = "#7F27A2";
+export const Primary200: string = "#C379E0";
+export const Primary100: string = "#E3C3FF";
+
+export const Secondary500: string = "#E23532";
+export const Secondary400: string = "#FC5C5A";
+export const Secondary300: string = "#FF8886";
+export const Secondary200: string = "#FFBAB6";
+export const Secondary100: string = "#FFDAD5";
+
+export const Neutral500: string = "#232F34";
+export const Neutral400: string = "#4A6572";
+export const Neutral300: string = "#88A7B7";
+export const Neutral200: string = "#EAF7FF";
+export const Neutral100: string = "#F9FDFF";
+
+// Special Colors
+export const BackgroundWithOpacity: string = "rgba(19, 3, 51, 0.7)";
 
 // Responsive breakpoints
 export const Mobile: string = "640px";
+
+// Spacing
+export const SpacingS: string = "16px";
+export const SpacingM: string = "32px";
+export const SpacingL: string = "64px";
