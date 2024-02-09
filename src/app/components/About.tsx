fc927b74f9cc39617b81b3650f4b3f7699e55fbf
EncryptEx <limusina10@gmail.com>
2024-02-09 12:04:20 +0100

    added provisional smoke

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -30,56 +30,78 @@ const ImgSection = styled.div`
   margin-bottom: ${SpacingS};
 `;
 
+const Smoke = styled(Image)`
+  width: 100%;
+  height: auto;
+`;
+const FixedSmoke = styled(Smoke)`
+  margin-bottom:-5px;
+`;
+
 export default function About() {
   return (
-    <SectionBackground specialBackground={"rgba(234, 247, 255, 0.8)"}>
-      <SectionStyled>
-        <SectionTitle>HackUPC beyond the stars</SectionTitle>
-        <Container>
-          <div>
-            <ImgSection>
-              <Image
-                src="/saturnPlanet.svg"
-                width={110}
-                height={110}
-                alt="Saturn Planet"
-              />
-            </ImgSection>
-            <BlockTitle color={Primary500} haveMargin>
-              What is a hackathon?
-            </BlockTitle>
-            <Body>
-              The word hackathon is a combination of the words "<b>HACK</b>",
-              standing for creative problem-solving, and "<b>MARATHON</b>".
-              These are awesome events full of creativity, technology, and
-              passionate tech-related students of different skill levels
-              collaborating and creating. During a certain period of time,
-              attendees work in <b>teams</b> to develop apps, games, robots…
-              literally anything you want to <b>build & learn</b> about!
-            </Body>
-          </div>
-          <div>
-            <ImgSection>
-              <Image
-                src="/hackupcLogo.svg"
-                width={90}
-                height={90}
-                alt="HackUPC Logo"
-              />
-            </ImgSection>
-            <BlockTitle color={Primary500} haveMargin>
-              About HackUPC?
-            </BlockTitle>
-            <Body>
-              HackUPC is the first student hackathon in Spain. Once a year, we
-              aim to provide students of <b>all experience levels</b> from{" "}
-              <b>all around the world</b> with a space where they can improve
-              and learn new skills, meet awesome people, and have an overall
-              great experience!
-            </Body>
-          </div>
-        </Container>
-      </SectionStyled>
-    </SectionBackground>
+    <div>
+      <FixedSmoke
+        src="/smoke_start.svg"
+        width={1000}
+        height={1000}
+        alt="Rocket Smoke"
+      />
+      <SectionBackground specialBackground={"rgba(234, 247, 255, 1)"}>
+        <SectionStyled>
+          <SectionTitle>HackUPC beyond the stars</SectionTitle>
+          <Container>
+            <div>
+              <ImgSection>
+                <Image
+                  src="/saturnPlanet.svg"
+                  width={110}
+                  height={110}
+                  alt="Saturn Planet"
+                />
+              </ImgSection>
+              <BlockTitle color={Primary500} haveMargin>
+                What is a hackathon?
+              </BlockTitle>
+              <Body>
+                The word hackathon is a combination of the words "<b>HACK</b>",
+                standing for creative problem-solving, and "<b>MARATHON</b>".
+                These are awesome events full of creativity, technology, and
+                passionate tech-related students of different skill levels
+                collaborating and creating. During a certain period of time,
+                attendees work in <b>teams</b> to develop apps, games, robots…
+                literally anything you want to <b>build & learn</b> about!
+              </Body>
+            </div>
+            <div>
+              <ImgSection>
+                <Image
+                  src="/hackupcLogo.svg"
+                  width={90}
+                  height={90}
+                  alt="HackUPC Logo"
+                />
+              </ImgSection>
+              <BlockTitle color={Primary500} haveMargin>
+                About HackUPC?
+              </BlockTitle>
+              <Body>
+                HackUPC is the first student hackathon in Spain. Once a year, we
+                aim to provide students of <b>all experience levels</b> from{" "}
+                <b>all around the world</b> with a space where they can improve
+                and learn new skills, meet awesome people, and have an overall
+                great experience!
+              </Body>
+            </div>
+          </Container>
+        </SectionStyled>
+      </SectionBackground>
+      <Smoke
+        src="/smoke_end.svg"
+        width={1000}
+        height={1000}
+        alt="Rocket Smoke"
+      />
+    </div>
   );
 }
