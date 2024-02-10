5cff86a2d22f8667f738b8d35d60b13028156f5c
carlotacb <carkbra@gmail.com>
2024-02-10 21:22:54 +0100

    added live dot in the live title

--- src/app/components/Live.tsx
+++ src/app/components/Live.tsx
@@ -6,20 +6,51 @@ import {
   SectionTitle,
 } from "@/app/genericComponents/Fonts";
 import { PrimaryButton } from "@/app/genericComponents/Buttons";
-import { SpacingS, SpacingXS } from "@/app/genericComponents/tokens";
-import styled from "styled-components";
+import {
+  Secondary500,
+  SpacingS,
+  SpacingXS,
+} from "@/app/genericComponents/tokens";
+import styled, { keyframes } from "styled-components";
+
+const live = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: .25;
+  }
+`;
 
 const TextContainer = styled.div`
   text-align: center;
   margin-bottom: ${SpacingS};
 `;
 
+const StyledSectionTitle = styled(SectionTitle)`
+  display: flex;
+  align-items: center;
+  gap: ${SpacingS};
+`;
+
+const LiveDotAnimated = styled.div`
+  -webkit-animation: ${live} 2.5s cubic-bezier(0.82, 0.23, 0.55, 0.95) infinite
+    alternate;
+  animation: ${live} 2.5s cubic-bezier(0.82, 0.23, 0.55, 0.95) infinite
+    alternate;
+  background: ${Secondary500};
+  border-radius: 100%;
+  height: ${SpacingS};
+  width: ${SpacingS};
+`;
+
 export default function Live() {
   return (
     <Section>
-      <div>
-        <SectionTitle>Live</SectionTitle>
-      </div>
+      <StyledSectionTitle>
+        <LiveDotAnimated />
+        Live
+      </StyledSectionTitle>
       <TextContainer>
         <BodyBold style={{ marginBottom: SpacingXS }}>
           When and where will everything happen?
