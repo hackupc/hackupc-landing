adc7027aa4cfe90c870fdb6555f345b5f9997cef
carlotacb <carkbra@gmail.com>
2024-02-11 12:30:24 +0100

    try to fix white glitch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,8 @@ const Background = styled.div`
   z-index: -1;
   width: 100%;
   height: 100%;
+  min-height: 100vh;
+  min-width: 100vw;
   background-image: url("/stars.jpeg");
   background-size: cover;
   background-repeat: no-repeat;
