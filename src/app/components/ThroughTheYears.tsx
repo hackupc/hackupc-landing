015bf7038644e867ea651453a26ed41da1adcce8
Achraf Hmimou <aacraaf@gmail.com>
2024-03-19 23:04:43 +0100

    EmblaCarousel

--- src/app/components/ThroughTheYears.tsx
+++ src/app/components/ThroughTheYears.tsx
@@ -1,16 +1,119 @@
+import React from "react";
+
+import styled, { keyframes } from "styled-components";
+import { Section, SectionBackground } from "@/app/genericComponents/General";
+import { SectionTitle } from "@/app/genericComponents/Typography";
+import { MaxScreenSize } from "@/app/genericComponents/tokens";
+import Image from "next/image";
+import useEmblaCarousel from "embla-carousel-react";
+import EmblaCarousel from "./ThroughTheYears/EmblaCarousel";
+import { EmblaOptionsType } from "embla-carousel";
+
+//import "./ThroughTheYears/css/base.css";
+
+import "./ThroughTheYears/css/embla.css";
+
+//import "./ThroughTheYears/css/sandbox.css";
+
+const Bullet = styled.span`
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  background-color: lightslategrey;
+  border-radius: 50%;
+  margin: 0 5px;
+
+  &:hover {
+    background-color: orangered;
+  }
+
+  &.Wactive {
+    background-color: orangered;
+  }
+`;
+
+const Edition = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  scroll-snap-align: start;
+`;
+
 export default function ThroughTheYears() {
+  const PREVIOUS_EDITIONS = [
+    {
+      name: "Fall 2016",
+      date: "October 23-25",
+      img: "/Planets/planetPartner.svg",
+      url: "",
+    },
+    {
+      name: "Winter 2016",
+      date: "May 8-12",
+      img: "/Planets/planetSponsor.svg",
+      url: "",
+    },
+    {
+      name: "Fall 2017",
+      date: "May 3-5",
+      img: "/Planets/planetMentor.svg",
+      url: "",
+    },
+    {
+      name: "Winter 2017",
+      date: "May 3-5",
+      img: "/Planets/planetVolunteer.svg",
+      url: "",
+    },
+    {
+      name: "2018",
+      date: "May 3-5",
+      img: "/Planets/planetPartner.svg",
+      url: "https://2018.hackupc.com/",
+    },
+    {
+      name: "2019",
+      date: "May 3-5",
+      img: "/Planets/planetSponsor.svg",
+      url: "https://2019.hackupc.com/",
+    },
+    {
+      name: "2020",
+      date: "CANCELLED",
+      img: "/Planets/planetMentor.svg",
+      url: "",
+    },
+    {
+      name: "2021",
+      date: "May 3-5",
+      img: "/Planets/planetVolunteer.svg",
+      url: "https://2021.hackupc.com/",
+    },
+    {
+      name: "2022",
+      date: "May 3-5",
+      img: "/Planets/planetVolunteer.svg",
+      url: "https://2022.hackupc.com/",
+    },
+    {
+      name: "2023",
+      date: "May 3-5",
+      img: "/Planets/planetVolunteer.svg",
+      url: "https://2023.hackupc.com/",
+    },
+  ];
+
+  const [emblaRef] = useEmblaCarousel();
+  const OPTIONS: EmblaOptionsType = {
+    align: "start",
+    loop: false,
+    slidesToScroll: 2,
+  };
+
   return (
-    <div
-      style={{
-        height: "200px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "2rem",
-      }}
-    >
-      {" "}
-      Coming soon...{" "}
-    </div>
+    <Section>
+      <SectionTitle>Through The Years</SectionTitle>
+      <EmblaCarousel slides={PREVIOUS_EDITIONS} options={OPTIONS} />
+    </Section>
   );
 }
---
+++ src/app/components/ThroughTheYears/EmblaCarousel.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { EmblaOptionsType } from "embla-carousel";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons,
+} from "./EmblaCarouselArrowButtons";
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
+import Link from "next/link";
+
+type PropType = {
+  slides: number[];
+  options?: EmblaOptionsType;
+};
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  return (
+    <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((edition, index) => (
+            <div className="embla__slide" key={index}>
+              <Link href={edition.url} target="_blank">
+                <Image
+                  src={edition.img}
+                  alt="altText"
+                  width="150"
+                  height="150"
+                />
+                <h3>{edition.name}</h3>
+                <p>{edition.date}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="embla__controls">
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={"embla__dot".concat(
+                index === selectedIndex ? " embla__dot--selected" : "",
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EmblaCarousel;
