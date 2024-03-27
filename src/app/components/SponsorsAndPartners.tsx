99f6ea4225a3ef33f57e8dc6f56a5ad804f13c11
Carlota Catot Bragós <carkbra@gmail.com>
2024-03-27 16:37:04 +0100

    Develop to master (#61)
    Co-authored-by: Adri <119002051+AdriMM26@users.noreply.github.com>
    Co-authored-by: Adrià Martínez <adria.martinez@build38.com>

--- src/app/components/SponsorsAndPartners.tsx
+++ src/app/components/SponsorsAndPartners.tsx
@@ -59,7 +59,11 @@ export default function SponsorsAndPartners() {
             />
           </MegaBox>
           <MegaBox>
-            <SponsorUnrevealed />
+            <SponsorLogo
+              imageSrc={"/Sponsors/inditex.svg"}
+              imageAlt={"inditex"}
+              sponsorLink={"https://www.zaratalent.com/es/tech/"}
+            />
           </MegaBox>
           <MegaBox>
             <SponsorUnrevealed />
