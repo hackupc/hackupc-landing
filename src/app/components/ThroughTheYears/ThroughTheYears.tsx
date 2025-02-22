ec26a8c1ba3a85200c171b17f33681c7bb944a82
alvarodr21 <alvarodr21@gmail.com>
2025-02-22 23:42:31 +0100

    :sparkles: Statistics and next containers

--- src/app/components/ThroughTheYears/ThroughTheYears.tsx
+++ src/app/components/ThroughTheYears/ThroughTheYears.tsx
@@ -1,15 +1,66 @@
 import React from "react";
 
 import { Section, SectionBackground } from "@/app/genericComponents/General";
-import { SectionTitle } from "@/app/genericComponents/Typography";
+import { SectionTitle, SmallTitle } from "@/app/genericComponents/Typography";
 import styled from "styled-components";
 import { silkscreen } from "@/app/genericComponents/fonts";
+
+const GameContainer = styled.div`
+  display: flex;
+  justify-content: center; /* Center Tetris container horizontally */
+  align-items: center; /* Align containers vertically */
+  position: relative;
+  width: 100%;
+  height: 750px; /* Full height of the viewport */
+`;
 
 const TetrisContainer = styled.div`
   position: relative;
   width: 250px;
   height: 750px;
   background-color: #000;
+`;
+
+const StatisticsContainer = styled.div`
+  display: none;
+  width: 200px;
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 20px;
+  position: relative;
+
+  @media (min-width: 800px) {
+    display: block;
+    align-self: flex-end;
+  }
+`;
+
+const StatisticsItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  justify-content: space-between;
+`;
+
+const PieceImage = styled.img<{ w: number }>`
+  width: ${(props) => props.w}px;
+  height: auto;
+`;
+
+const NextContainer = styled.div`
+  display: none;
+  width: 200px;
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 20px;
+  position: relative;
+
+  @media (min-width: 800px) {
+    display: block;
+    align-self: flex-start; /* Align with the top of the Tetris container */
+  }
 `;
 
 const TetrisPiece = styled.img<{ x: number; y: number; w: number }>`
@@ -28,140 +79,192 @@ export default function ThroughTheYears() {
           <SectionTitle className={silkscreen.className}>
             Through The Years
           </SectionTitle>
-          <TetrisContainer>
-            <a
-              href="https://2024.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2024.svg"
-                x={200}
-                y={50}
-                w={50}
-              />
-            </a>
-            <a
-              href="https://2023.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2023.svg"
-                x={0}
-                y={198}
-                w={100}
-              />
-            </a>
-            <a
-              href="https://2022.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2022.svg"
-                x={0}
-                y={299}
-                w={150}
-              />
-            </a>
-            <a
-              href="https://2021.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2021.svg"
-                x={101}
-                y={300}
-                w={100}
-              />
-            </a>
-            <a
-              href="https://2020.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2020.svg"
-                x={0}
-                y={350}
-                w={99}
-              />
-            </a>
-            <a
-              href="https://2019.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2019.svg"
-                x={0}
-                y={450}
-                w={150}
-              />
-            </a>
-            <a
-              href="https://2018.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2018.svg"
-                x={150}
-                y={450}
-                w={100}
-              />
-            </a>
-            <a
-              href="https://2017w.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2017w.svg"
-                x={1}
-                y={553}
-                w={100}
-              />
-            </a>
-            <a
-              href="https://2017f.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2017f.svg"
-                x={100}
-                y={551}
-                w={150}
-              />
-            </a>
-            <a
-              href="https://2026w.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2016w.svg"
-                x={149}
-                y={603}
-                w={100}
-              />
-            </a>
-            <a
-              href="https://2016s.hackupc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TetrisPiece
-                src="/ThroughTheYears/tetris-2016s.svg"
-                x={0}
-                y={649}
-                w={147}
-              />
-            </a>
-          </TetrisContainer>
+          <GameContainer>
+            <StatisticsContainer>
+              <SmallTitle className={silkscreen.className}>
+                Statistics
+              </SmallTitle>
+              <div style={{ height: "20px" }} />
+              <StatisticsItem>
+                <PieceImage src="/ThroughTheYears/tetris-basic1.svg" w={30} />
+                <span>Editions: 0010</span>
+              </StatisticsItem>
+              <StatisticsItem>
+                <PieceImage src="/ThroughTheYears/tetris-basic5.svg" w={30} />
+                <span>Hackers: 5344</span>
+              </StatisticsItem>
+              <StatisticsItem>
+                <PieceImage src="/ThroughTheYears/tetris-basic2.svg" w={30} />
+                <span>Projects: 1102</span>
+              </StatisticsItem>
+              <StatisticsItem>
+                <PieceImage src="/ThroughTheYears/tetris-basic3.svg" w={8} />
+                <span>Volunteers: 0416</span>
+              </StatisticsItem>
+              <StatisticsItem>
+                <PieceImage src="/ThroughTheYears/tetris-basic4.svg" w={20} />
+                <span>Prizes: 0352</span>
+              </StatisticsItem>
+            </StatisticsContainer>
+            <TetrisContainer>
+              <a
+                href="https://2024.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2024.svg"
+                  x={200}
+                  y={50}
+                  w={50}
+                />
+              </a>
+              <a
+                href="https://2023.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2023.svg"
+                  x={0}
+                  y={198}
+                  w={100}
+                />
+              </a>
+              <a
+                href="https://2022.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2022.svg"
+                  x={0}
+                  y={299}
+                  w={150}
+                />
+              </a>
+              <a
+                href="https://2021.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2021.svg"
+                  x={101}
+                  y={300}
+                  w={100}
+                />
+              </a>
+              <a
+                href="https://2020.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2020.svg"
+                  x={0}
+                  y={350}
+                  w={99}
+                />
+              </a>
+              <a
+                href="https://2019.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2019.svg"
+                  x={0}
+                  y={450}
+                  w={150}
+                />
+              </a>
+              <a
+                href="https://2018.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2018.svg"
+                  x={150}
+                  y={450}
+                  w={100}
+                />
+              </a>
+              <a
+                href="https://2017w.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2017w.svg"
+                  x={1}
+                  y={553}
+                  w={100}
+                />
+              </a>
+              <a
+                href="https://2017f.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2017f.svg"
+                  x={100}
+                  y={551}
+                  w={150}
+                />
+              </a>
+              <a
+                href="https://2026w.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2016w.svg"
+                  x={149}
+                  y={603}
+                  w={100}
+                />
+              </a>
+              <a
+                href="https://2016s.hackupc.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <TetrisPiece
+                  src="/ThroughTheYears/tetris-2016s.svg"
+                  x={0}
+                  y={649}
+                  w={147}
+                />
+              </a>
+            </TetrisContainer>
+            <NextContainer>
+              <SmallTitle className={silkscreen.className}>Next</SmallTitle>
+              <div style={{ height: "20px" }} />
+              <PieceImage w={100} src="/ThroughTheYears/tetris-2025.svg" />
+              <div style={{ height: "20px" }} />
+              <div
+                style={{
+                  position: "relative",
+                  width: "100px",
+                  height: "100px",
+                }}
+              >
+                <PieceImage
+                  w={120}
+                  src="/ThroughTheYears/tetris-next.svg"
+                  style={{ position: "absolute", top: 30, left: 20 }}
+                />
+                <PieceImage
+                  w={60}
+                  src="/ThroughTheYears/question.png"
+                  style={{ position: "absolute", top: 0, left: 50 }}
+                />
+              </div>
+            </NextContainer>
+          </GameContainer>
         </Section>
       </SectionBackground>
     </div>
