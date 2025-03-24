53ceab3884f6e46cc2945a071c300542e9a9233d
AwaKT <60238912+gerardm27@users.noreply.github.com>
2025-03-24 15:01:29 +0100

    Added frit and risi (#158)

--- src/app/components/SponsorsAndPartners.tsx
+++ src/app/components/SponsorsAndPartners.tsx
@@ -318,6 +318,11 @@ export default function SponsorsAndPartners() {
             />
           </MiliBox>
           <MiliBox>
+            <SponsorUnrevealed />
+          </MiliBox>
+        </SponsorRow>
+        <SponsorRow>
+          <MiliBox>
             <SponsorLogo
               imageSrc={"/Partners/nissin.png"}
               imageAlt={"nissin"}
@@ -337,6 +342,22 @@ export default function SponsorsAndPartners() {
               imageSrc={"/Partners/xurreria_manolo.png"}
               imageAlt={"Xurreria Manolo"}
               sponsorLink={"https://www.instagram.com/xurreriacalmanolo"}
+              smallPadding
+            />
+          </MiliBox>
+          <MiliBox>
+            <SponsorLogo
+              imageSrc={"/Partners/risi.png"}
+              imageAlt={"RISI"}
+              sponsorLink={"https://risi.es/es/"}
+              smallPadding
+            />
+          </MiliBox>
+          <MiliBox>
+            <SponsorLogo
+              imageSrc={"/Partners/frit.png"}
+              imageAlt={"Frit Ravich"}
+              sponsorLink={"https://www.fritravich.com/en/"}
               smallPadding
             />
           </MiliBox>
