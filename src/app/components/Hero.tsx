54b43bbb2542b14c6a5e371696b4f82f65afa52b
polmf <99polmf@gmail.com>
2026-01-18 00:50:02 +0100

    millores

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,21 +1,14 @@
 import Image from "next/image";
 import styled, { keyframes } from "styled-components";
-import {
-  MobileBreakpoint,
-  SpacingM,
-  SpacingS,
-  SpacingXL,
-  TitleS,
-} from "@/app/genericComponents/tokens";
+import { MobileBreakpoint } from "@/app/genericComponents/tokens";
 import { montserrat } from "@/app/genericComponents/fonts";
 
 const Colors = {
   SkyBlue: "#AADEFE",
   MedievalGreen: "#75AA7E",
   Black: "#000000",
-  StoneGrey: "#D9D9D9", 
+  StoneGrey: "#D9D9D9",
 };
-
 
 const float = keyframes`
   0% { transform: translateY(0px); }
@@ -29,31 +22,31 @@ const fly = keyframes`
   100% { transform: translateX(0px) translateY(0px); }
 `;
 
-
 const HeroContainer = styled.div`
   position: relative;
-  overflow-x: hidden;
-  overflow-y: hidden;
-  display: flex;
-  flex-direction: column;
   width: 100vw;
-  height: 100vh;
+  min-height: 120vh;
   background-color: ${Colors.SkyBlue};
+  overflow-x: hidden;
+
+  @media (max-width: ${MobileBreakpoint}) {
+    min-height: 100vh;
+  }
 `;
 
 const SkySection = styled.div`
-  height: 45%;
+  height: 70vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-end; 
+  justify-content: flex-end;
   position: relative;
   width: 100%;
-  padding-bottom: 60px; 
+  padding-bottom: 80px;
 
   @media (max-width: ${MobileBreakpoint}) {
-    padding-bottom: 50px;
-    height: 42%;
+    height: 58vh;
+    padding-bottom: 60px;
   }
 `;
 
@@ -61,83 +54,99 @@ const DragonWrapper = styled.div`
   position: absolute;
   z-index: 2;
   top: 15%;
-  right: 18%; /* Movido un poco más a la izquierda para separarlo de la nube derecha */
-  width: 160px;
+  right: 18%;
+  width: 250px;
   animation: ${fly} 5s ease-in-out infinite;
 
   @media (max-width: ${MobileBreakpoint}) {
-    top: 50px;
-    right: auto;
-    left: 10px;
-    width: 75px;
+    top: 120px;      
+    right: 10px;     
+    left: auto;
+    width: 105px;
   }
 `;
 
 const BaseCloud = styled.div`
   position: absolute;
-  z-index: 1; 
+  z-index: 1;
   opacity: 0.9;
   animation: ${float} 6s ease-in-out infinite;
   pointer-events: none;
 `;
 
 const LeftCloud = styled(BaseCloud)`
-  /* ESCRITORIO: Pegada al borde izquierdo */
-  top: 18%;
-  left: 2%; 
+  top: 48%;
+  left: 2%;
   width: 140px;
 
-  @media (max-width: ${MobileBreakpoint}) {
-    /* MÓVIL: Asomando desde fuera de la pantalla */
-    top: 90px;
-    left: -40px; /* Valor negativo para que solo se vea la mitad */
-    width: 110px; 
-    opacity: 0.8;
+  @media (max-width: ${MobileBreakpoint}) { 
+    left: -40px;
+    width: 110px;
   }
 `;
 
 const RightCloud = styled(BaseCloud)`
-  /* ESCRITORIO: Pegada al borde derecho */
-  top: 28%;
+  top: 38%;
   right: 2%;
   width: 120px;
 
   @media (max-width: ${MobileBreakpoint}) {
-    /* MÓVIL: Asomando desde fuera a la derecha y más abajo */
-    top: 160px;
-    right: -30px; 
+  top: 58%;
+    right: -30px;
     width: 90px;
-    opacity: 0.8;
   }
 `;
 
+const CenterCloudLeft = styled(BaseCloud)`
+  top: 22%;
+  left: 32%;
+  width: 180px;
+
+  @media (max-width: ${MobileBreakpoint}) {
+    display: none;    
+  }
+`;
+
+const CenterCloudRight = styled(BaseCloud)`
+  top: 60%;
+  right: 20%;
+  width: 115px;
+
+  @media (max-width: ${MobileBreakpoint}) {
+    display: none;    
+  }
+`;
 
 const LogoContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  z-index: 10;
-  margin-bottom: 10px; 
+  z-index: 2;
   width: 100%;
-  position: relative; /* Asegura z-index */
 `;
 
 const WallSection = styled.div`
-  height: 55%;
+  min-height: 70vh;
   width: 100%;
   position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   background-image: url("/muralla.svg");
+  background-repeat: repeat-x;
+  padding-top: 120px;
+
+  @media (max-width: ${MobileBreakpoint}) {
+    min-height: 50vh;
+  }
 `;
 
 const RibbonContainer = styled.div`
   position: absolute;
-  top: 0;
+  top: 150px;
   left: 50%;
-  transform: translate(-50%, -50%); 
-  z-index: 20; 
+  transform: translateX(-50%);
+  z-index: 1;
   width: 100%;
   display: flex;
   justify-content: center;
@@ -145,16 +154,16 @@ const RibbonContainer = styled.div`
 
 const CharacterGrid = styled.div`
   display: flex;
-  gap: 80px; 
-  margin-top: 50px; 
-  z-index: 10;
+  gap: 100px;
+  margin-top: 120px;
+  z-index: 1;
   width: 100%;
   justify-content: center;
   align-items: flex-end;
 
   @media (max-width: ${MobileBreakpoint}) {
-    gap: 15px; 
-    margin-top: 40px;
+    gap: 20px;
+    margin-top: 80px;
   }
 `;
 
@@ -165,14 +174,14 @@ const CharacterCard = styled.a`
   align-items: center;
   text-decoration: none;
   transition: transform 0.2s ease;
-  width: 180px; 
+  width: 180px;
 
   &:hover {
     transform: translateY(-5px);
   }
 
   @media (max-width: ${MobileBreakpoint}) {
-    width: 42%; /* Un poco más ancho en móvil */
+    width: 42%;
     max-width: 150px;
   }
 `;
@@ -180,11 +189,11 @@ const CharacterCard = styled.a`
 const StackedImages = styled.div`
   position: relative;
   width: 100%;
-  height: 190px; 
+  height: 190px;
   display: flex;
-  justify-content: center; 
-  align-items: flex-end; 
-  
+  justify-content: center;
+  align-items: flex-end;
+
   @media (max-width: ${MobileBreakpoint}) {
     height: 140px;
   }
@@ -192,8 +201,7 @@ const StackedImages = styled.div`
 
 const ArchBg = styled(Image)`
   position: absolute;
-  top: 0;
-  left: 0;
+  inset: 0;
   z-index: 1;
   object-fit: contain;
   object-position: bottom;
@@ -202,10 +210,10 @@ const ArchBg = styled(Image)`
 const CharacterImg = styled(Image)`
   position: relative;
   z-index: 2;
-  width: 70%; 
+  width: 70%;
   height: auto;
-  margin-bottom: 30px; 
-  filter: drop-shadow(0 5px 5px rgba(0,0,0,0.2)); 
+  margin-bottom: 30px;
+  filter: drop-shadow(0 5px 5px rgba(0,0,0,0.2));
 
   @media (max-width: ${MobileBreakpoint}) {
     margin-bottom: 20px;
@@ -214,17 +222,17 @@ const CharacterImg = styled(Image)`
 
 const CharacterLabel = styled.div`
   background-color: ${Colors.StoneGrey};
-  width: 100%; 
+  width: 100%;
   padding: 10px 0;
   text-align: center;
   color: ${Colors.Black};
   font-weight: 900;
   font-family: ${montserrat.style.fontFamily};
-  font-size: 14px; 
+  font-size: 14px;
   letter-spacing: 1px;
-  z-index: 3;
+  z-index: 1;
   border-radius: 4px;
-  box-shadow: 0 5px 0 #999; 
+  box-shadow: 0 5px 0 #999;
   text-transform: uppercase;
   margin-top: -12px;
 
@@ -239,48 +247,33 @@ export default function Hero() {
     <HeroContainer>
       <SkySection>
         <LeftCloud>
-          <Image 
-            src="/cloud.svg" 
-            width={140} 
-            height={85} 
-            alt="Cloud" 
-            style={{ width: '100%', height: 'auto' }} 
-          />
+          <Image src="/cloud.svg" width={140} height={85} alt="Cloud" />
         </LeftCloud>
+
+        <CenterCloudLeft>
+          <Image src="/cloud.svg" width={180} height={95} alt="Cloud" />
+        </CenterCloudLeft>
+
+        <CenterCloudRight>
+          <Image src="/cloud.svg" width={120} height={70} alt="Cloud" />
+        </CenterCloudRight>
+
         <RightCloud>
-          <Image 
-            src="/cloud.svg" 
-            width={110} 
-            height={70} 
-            alt="Cloud" 
-            priority
-            style={{ width: '100%', height: 'auto' }} 
-          />
+          <Image src="/cloud.svg" width={110} height={70} alt="Cloud" />
         </RightCloud>
-        
+
         <DragonWrapper>
-          <Image 
-            src="/dragon.svg" 
-            width={160} 
-            height={120} 
-            alt="Dragon" 
-            style={{ width: "100%", height: "auto" }}
-          />
+          <Image src="/dragon.svg" width={160} height={120} alt="Dragon" />
         </DragonWrapper>
 
         <LogoContainer>
           <Image
             src="/main_logo.svg"
-            width={550} 
+            width={550}
             height={250}
             alt="Hack UPC"
             priority
-            style={{ 
-              maxWidth: "90vw", 
-              maxHeight: "35vh", 
-              width: "auto", 
-              height: "auto" 
-            }}
+            style={{ maxWidth: "90vw", height: "auto" }}
           />
         </LogoContainer>
       </SkySection>
@@ -299,34 +292,16 @@ export default function Hero() {
         <CharacterGrid>
           <CharacterCard href="https://my.hackupc.com/user/signup/hacker/" target="_blank">
             <StackedImages>
-                <ArchBg 
-                    src="/arch.svg" 
-                    fill 
-                    alt="Arch Background" 
-                />
-                <CharacterImg 
-                    src="/hacker.svg" 
-                    width={150} 
-                    height={150} 
-                    alt="Hacker" 
-                />
+              <ArchBg src="/arch.svg" fill alt="Arch" />
+              <CharacterImg src="/hacker.svg" width={150} height={150} alt="Hacker" />
             </StackedImages>
             <CharacterLabel>HACKER</CharacterLabel>
           </CharacterCard>
 
           <CharacterCard href="https://my.hackupc.com/user/signup/volunteer/" target="_blank">
             <StackedImages>
-                <ArchBg 
-                    src="/arch.svg" 
-                    fill 
-                    alt="Arch Background" 
-                />
-                <CharacterImg 
-                    src="/volunteer.svg" 
-                    width={150} 
-                    height={150} 
-                    alt="Volunteer" 
-                />
+              <ArchBg src="/arch.svg" fill alt="Arch" />
+              <CharacterImg src="/volunteer.svg" width={150} height={150} alt="Volunteer" />
             </StackedImages>
             <CharacterLabel>VOLUNTEER</CharacterLabel>
           </CharacterCard>
