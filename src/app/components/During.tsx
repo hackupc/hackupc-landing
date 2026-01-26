b9ab90e975c039b0b4df348c55af53be3fcaedf2
enric4000 <enric.andujar@gmail.com>
2026-01-26 15:54:39 +0100

    Mission Ribbon background added

--- src/app/components/During.tsx
+++ src/app/components/During.tsx
@@ -31,7 +31,7 @@ const TextWrapper = styled.div`
   justify-content: center;
   gap: ${SpacingS};
   display: inline-flex;
-  margin-top: ${SpacingM};
+  margin-top: ${SpacingS};
   margin-bottom: ${SpacingS};
 `;
 
@@ -46,6 +46,11 @@ const TheMission = styled.div`
   flex-direction: column;
   position: relative;
   text-align: center;
+  padding: 0 150px 0 150px;
+
+  @media (max-width: 1000px) {
+    padding: 0 15% 0 15%;
+  }
 `;
 
 const MapImageBase = styled.img`
@@ -79,6 +84,76 @@ const DesktopMapImage = styled(MapImageBase)`
   }
 `;
 
+export const RibbonTitle = styled(SectionTitle)`
+  position: relative;
+  color: #000000;
+  width: fit-content;
+  margin: 0 auto 45px auto;
+  padding: 8px 32px 24px 32px;
+  font-size: 32px;
+  background-color: rgba(238, 228, 194, 1);
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+
+  /* Left */
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: -20px;
+    left: -100px;
+    width: 40%;
+    height: 80%;
+    background-color: #a19673ff;
+    z-index: -1;
+    clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%, 20% 50%);
+  }
+
+  /* Right */
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: -20px;
+    right: -100px;
+    width: 40%;
+    height: 80%;
+    background-color: #a19673ff;
+    z-index: -1;
+    clip-path: polygon(0% 0%, 100% 0%, 80% 50%, 100% 100%, 0% 100%);
+  }
+
+  @media (max-width: 1000px and min-width: ${MobileBreakpoint}) {
+    &::before {
+      left: -100px;
+      width: 50%;
+      height: 50%;
+    }
+
+    /* Right */
+    &::after {
+      right: -100px;
+      width: 50%;
+      height: 50%;
+    }
+  }
+  @media (max-width: ${MobileBreakpoint}) {
+    &::before {
+      display: none;
+      left: -75px;
+      width: 50%;
+      height: 60%;
+    }
+
+    /* Right */
+    &::after {
+      display: none;
+      right: -75px;
+      width: 50%;
+      height: 60%;
+    }
+  }
+`;
+
 export default function During() {
   return (
     <DuringContainer id="during">
@@ -93,17 +168,19 @@ export default function During() {
       <DesktopMapImage src="/map_desktop.svg" alt="Map Desktop" />
 
       <TheMission>
-        <TextWrapper>
-          <StyledTitle className={lora.className}>
-            THE MISSION: HACKUPC
-          </StyledTitle>
-        </TextWrapper>
-        <Body>
-          Your weekend quest awaits! This year, HackUPC becomes a medieval
-          adventure filled with challenges to conquer. Forge your own path by
-          overcoming mighty trials, and see if you can rise as a true hero. Will
-          your legend be told in generations?
-        </Body>
+        <RibbonTitle className={lora.className}>
+          <TextWrapper>
+            <StyledTitle className={lora.className}>
+              THE MISSION: HACKUPC
+            </StyledTitle>
+          </TextWrapper>
+          <Body>
+            Your weekend quest awaits! This year, HackUPC becomes a medieval
+            adventure filled with challenges to conquer. Forge your own path by
+            overcoming mighty trials, and see if you can rise as a true hero.
+            Will your legend be told in generations?
+          </Body>
+        </RibbonTitle>
       </TheMission>
     </DuringContainer>
   );
